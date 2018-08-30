00618f7b382a2c0e89a8e2934d0b32b507fa159a
kenji <kenjaxzxc@gmail.com>
2018-08-31 07:34:36 +0800

    push

---
+++ public/adminaccept.php
@@ -0,0 +1,176 @@
+<?php 
+  include('connection.php'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>3RVillage</title>
+  <?php include('link.php');?>
+  </head>
+  
+<body class="bg-light">
+  <?php 
+      include('header.php');
+   ?>
+  
+  <div class="row px-5 py-3 border m-t-100">
+  <div class="col-sm-12">
+    <div class="row border">
+      <div class="col-sm-6  border">
+        <div class="row">
+          <label>Seller:</label>
+        </div>
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="row justify-content-center" >
+              <i class="fa fa-user-circle" ></i>
+            </div>
+          </div>
+          <div class="col-sm-10">
+            <div class="row">
+              <h5>Divina Gracia</h5>
+            </div>
+            <div class="row">
+              <label>Type: Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Name: Cherry Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Item Description: Dual-core processor, GB internal</label>
+            </div>
+            <div class="row">
+              <label>Price: 1,200</label>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center py-3">
+          <button type="button" class="btn btn-success">Accept</button>
+          <button type="button" class="btn btn-dark">View</button>
+          <button type="button" class="btn btn-danger">Decline</button>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="row justify-content-center" >
+              <i class="fa fa-user-circle" ></i>
+            </div>
+          </div>
+          <div class="col-sm-10">
+            <div class="row">
+              <h5>Khim Veer Lee</h5>
+            </div>
+            <div class="row">
+              <label>Type: Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Name: Cherry Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Item Description: Dual-core processor, GB internal</label>
+            </div>
+            <div class="row">
+              <label>Price: 1,200</label>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center py-3">
+          <button type="button" class="btn btn-success">Accept</button>
+          <button type="button" class="btn btn-dark">View</button>
+          <button type="button" class="btn btn-danger">Decline</button>
+        </div>
+        <div class="row justify-content-end p-2">
+          <button type="button" class="btn btn-default">Show More</button>
+        </div>
+      </div>
+
+      <div class="col-sm-6  border">
+        <div class="row">
+          <label>Donor:</label>
+        </div>
+        <div class="row px-3">
+          <label>You have been Awarded by:</label>
+        </div>
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="row justify-content-center" >
+              <i class="fa fa-user-circle" ></i>
+            </div>
+          </div>
+          <div class="col-sm-10">
+            <div class="row">
+              <h5>Francis Carlo Fontanilla</h5>
+            </div>
+            <div class="row">
+              <label>Type: Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Name: Cherry Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Item Description: Dual-core processor, GB internal</label>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center py-3">
+          <button type="button" class="btn btn-success">Accept</button>
+          <button type="button" class="btn btn-danger">Decline</button>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-12">
+            <label>You have been Awarded by:</label>
+          </div>
+          <div class="col-sm-2">
+            <div class="row justify-content-center" >
+              <i class="fa fa-user-circle" ></i>
+            </div>
+          </div>
+          <div class="col-sm-10">
+            <div class="row">
+              <h5>Leticia Gempesao</h5>
+            </div>
+            <div class="row">
+              <label>Type: Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Name: Cherry Mobile Phone</label>
+            </div>
+            <div class="row">
+              <label>Item Description: Dual-core processor, GB internal</label>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center py-3">
+          <button type="button" class="btn btn-success">Accept</button>
+          <button type="button" class="btn btn-danger">Decline</button>
+        </div>
+        <div class="row justify-content-end p-2">
+          <button type="button" class="btn btn-default">Show More</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  
+  
+    <?php
+      include('footer.php');
+    ?>
+
+ </body>
+</html>  
+
+  
+<!--===============================================================================================-->  
+  <script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../vendor/bootstrap/js/popper.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="../vendor/select2/select2.min.js"></script>
+
+  <script src="../js/main.js"></script>
+  
--- public/editprofile.php
+++ public/editprofile.php
@@ -13,7 +13,45 @@
       include('header.php');
    ?>
   
+  <section class="col-sm-12 bg-light">  
   
+  <div class="row">
+   <h4 class="ltext-102 cl5 m-t-100 m-l-100 m-b-30">Profile</h4>
+      
+  </div>
+  <form action="" method="POST">
+  <div class="row justify-content-center">
+    
+    <div class="col-sm-5">
+     
+      <div class="form-group">    
+        <label>Title </label>
+        <input class="form-control" type="text" name="title" required autofocus>
+      </div>
+      <?php 
+      include('category.php');
+      ?>
+      <div class="form-group">
+        <label>Description</label>
+        <textarea class="form-control" name="description" rows="3" required></textarea>
+      </div>
+      <?php 
+      include('location.php');
+      ?>
+      <div class="form-group">
+        <label>Price</label>
+        <input class="form-control" type="number" name="price" required>
+      </div>
+
+      <div class="mt-4 m-b-100">
+        <button type="submit" class="btn btn-outline-secondary float-right" id="btnCancel">Cancel</button>
+        <button type="submit" class="btn btn-success float-right mr-2" id="btnSave" name="btnSave">Save</button>
+      </div>
+    </div>
+
+    </div> 
+    </form>
+</section>
   
   
     <?php
